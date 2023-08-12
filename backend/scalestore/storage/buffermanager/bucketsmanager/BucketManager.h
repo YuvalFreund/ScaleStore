916d8991f8e1b2a2d4ec513b00aafa02ede7190b
“Yuval-F” <yuval@frnzx.io>
2023-08-12 12:20:28 +0200

    delete page, fixing includes

--- backend/scalestore/storage/buffermanager/bucketsmanager/BucketManager.h
+++ backend/scalestore/storage/buffermanager/bucketsmanager/BucketManager.h
@@ -13,7 +13,6 @@
 
 //files
 #include "Bucket.h"
-#include "tools.h"
 #include "cluster.h"
 #include "GenericDisjointSets.h"
 
@@ -347,6 +346,21 @@ public:
             itr.second.printBucketData();
         }
     }
+    static inline uint64_t FasterHash(uint64_t input) {
+        uint64_t local_rand = input;
+        uint64_t local_rand_hash = 8;
+        local_rand_hash = 40343 * local_rand_hash + ((local_rand) & 0xFFFF);
+        local_rand_hash = 40343 * local_rand_hash + ((local_rand >> 16) & 0xFFFF);
+        local_rand_hash = 40343 * local_rand_hash + ((local_rand >> 32) & 0xFFFF);
+        local_rand_hash = 40343 * local_rand_hash + (local_rand >> 48);
+        local_rand_hash = 40343 * local_rand_hash;
+        return local_rand_hash; // if 64 do not rotate
+        // else:
+        // return Rotr64(local_rand_hash, 56);
+    }
 
+    uint64_t tripleHash(uint64_t input){
+        return FasterHash(FasterHash(FasterHash(input)));
+    }
 
 };
